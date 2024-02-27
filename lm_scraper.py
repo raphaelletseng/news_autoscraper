c3f9925abe620fc83bd50b7d2ec3f1630807fdf2
rtseng <rtseng@ghgsat.com>
2024-02-27 10:01:05 -0500

    add le monde

--- lm_scraper.py
+++ lm_scraper.py
@@ -7,42 +7,24 @@ lm_url = "https://www.lemonde.fr/en/"
 lm_response = requests.get(lm_url)
 lm_soup = BeautifulSoup(lm_response.content, "html.parser")
 
-lm_headlines = lm_soup.select("p.article__title")
-print(lm_headlines)
+lm_second_headlines = lm_soup.select_one("section.area--headlines").select("p.article__title")
 data = {
     'org': lm_url,
     'scraped_at': datetime.datetime.now(),
-    'headline_1': lm_headlines[0].select('p.article__title-label')[0].text,
-    'headline_2': '',
-    'headline_3': '',
+    'headline_1': lm_soup.select_one("section.area--main").select_one("p.article__title-label"),
+    'headline_2': lm_second_headlines[0].text,
+    'headline_3': lm_second_headlines[1].text,
 }
 headlines=[]
-
-for idx, h in enumerate(lm_headlines[1:3]):
-    print('here')
-    try:
-        headline = h.select('p.article__title').text
-        print(headline)
-        print('----')
-        headlines.append(h.text)
-    except:
-        pass
-
-print(headlines)
-
-for i in range(1, len(headlines)):
-    key = f'headline_{i+1}'
-    value = headlines[i]
-    data[key] = value
 
 df = pd.DataFrame(data, index=[0])
 print(df.head())
 
-# try:
-#     existing_df = pd.read_csv("updated_headlines.csv")
-# except:
-#     existing_df = pd.DataFrame([])
+try:
+    existing_df = pd.read_csv("updated_headlines.csv")
+except:
+    existing_df = pd.DataFrame([])
 
-# combined = pd.concat([df, existing_df], ignore_index=True)
+combined = pd.concat([df, existing_df], ignore_index=True)
 
-# combined.to_csv("updated_headlines.csv", index=False)
+combined.to_csv("updated_headlines.csv", index=False)
