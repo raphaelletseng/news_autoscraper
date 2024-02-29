d07c45376469940129b57f766e8756a3bc74d7ce
rtseng <rtseng@ghgsat.com>
2024-02-29 16:34:43 -0500

    add links to aj

--- aj_scraper.py
+++ aj_scraper.py
@@ -16,13 +16,14 @@ data = {
     'headline_3': '',
 }
 headlines=[]
+links = []
 for idx, h in enumerate(aj_headlines[:4]):
     try:
         headline = (h.select('h3.article-card__title')[0].text.replace(u'\xad', ''))
         headlines.append(headline)
+        links.append(h.find('a', href=True))
     except:
         pass
-
 
 def dups(seq):
     seen = set()
@@ -30,12 +31,15 @@ def dups(seq):
     return [x for x in seq if not (x in seen or seen_add(x))]
 
 nHeadlines = dups(headlines)
+nLinks = dups(links)
 for i in range(0, len(nHeadlines)):
     key = f'headline_{i+1}'
-    value = nHeadlines[i]
+    value = str(nHeadlines[i]) + ", " + aj_url + str(nLinks[i]['href'])
+    print(value)
     data[key] = value
 
 df = pd.DataFrame(data, index=[0])
+print(df.head())
 
 try:
     existing_df = pd.read_csv("updated_headlines.csv")
